6fb10eed1e1d041c7cbc04bbe6f6b6658d13a936
Tobias Nobile <tobias.nobile@hotmail.com>
2026-05-13 18:17:58 +0200

    ok

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
 from solutions.problem_768.solution import *
 
 def tests():
-    assert rejection_sampling_best_of_k([['a','b','c']], [[0.1, 0.5, 0.3]]) != ["b"]
+    assert rejection_sampling_best_of_k([['a','b','c']], [[0.1, 0.5, 0.3]]) == ["b"]
