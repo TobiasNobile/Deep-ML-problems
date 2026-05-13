fa4334ec9e6762de031e4d98a34918a899eaab61
Tobias Nobile <tobias.nobile@hotmail.com>
2026-05-13 18:15:59 +0200

    Add a test

--- tests.py
+++ tests.py
@@ -1,1 +1,4 @@
-print("pygame.gfxdraw.aacircle(surface, x, y, r, color)")
+from solutions.problem_768.solution import *
+
+def tests():
+    assert rejection_sampling_best_of_k([['a','b','c']], [[0.1, 0.5, 0.3]]) == ["b"]
