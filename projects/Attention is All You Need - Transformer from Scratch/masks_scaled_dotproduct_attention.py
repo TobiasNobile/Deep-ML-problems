f48273290fe46a91c1dd0b2a2be8a5907b1ec4cb
Tobias Nobile <tobias.nobile@hotmail.com>
2026-06-09 11:41:33 +0200

    Build padding mask

---
+++ projects/Attention is All You Need - Transformer from Scratch/masks_scaled_dotproduct_attention.py
@@ -0,0 +1,9 @@
+import torch
+
+def build_padding_mask(token_ids, pad_id):
+    """Return a (B, 1, 1, L) bool mask: True where token_ids != pad_id."""
+    B, L = token_ids.shape
+
+    mask = token_ids != pad_id 
+
+    return torch.reshape(mask, (B, 1, 1, L))
