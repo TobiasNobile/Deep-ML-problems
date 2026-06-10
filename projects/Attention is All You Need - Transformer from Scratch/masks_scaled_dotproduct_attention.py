d3facd047a427881b64084bf7215014ba50602e7
Tobias Nobile <tobias.nobile@hotmail.com>
2026-06-10 10:55:31 +0200

    Combine padding and casual masks

--- projects/Attention is All You Need - Transformer from Scratch/masks_scaled_dotproduct_attention.py
+++ projects/Attention is All You Need - Transformer from Scratch/masks_scaled_dotproduct_attention.py
@@ -17,3 +17,9 @@ def build_causal_mask(seq_len):
                 mask[i, j] = True
 
     return torch.reshape(torch.tril(mask), (1, 1, seq_len, seq_len))
+
+def combine_padding_and_causal_masks(padding_mask, causal_mask):
+    """
+    Combine a (B,1,1,L) padding mask with a (1,1,L,L) causal mask into (B,1,L,L).
+    """
+    return padding_mask & causal_mask
