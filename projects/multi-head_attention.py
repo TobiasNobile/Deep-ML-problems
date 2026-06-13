38c202347fb1fe07eaa2afe1420aa05c4e879561
Tobias Nobile <tobias.nobile@hotmail.com>
2026-06-13 20:26:03 +0200

    Transpose heads before sequence

--- projects/multi-head_attention.py
+++ projects/multi-head_attention.py
@@ -6,3 +6,9 @@ def split_last_dim_into_heads(tensor, num_heads):
     """
     B, L, d_model = tensor.shape
     return torch.reshape(tensor, (B, L, num_heads, d_model // num_heads))
+
+def transpose_heads_before_sequence(split_tensor):
+    """
+    rearrange (B, L, num_heads, d_k) into (B, num_heads, L, d_k).
+    """
+    return torch.permute(split_tensor, (0, 2, 1, 3))
