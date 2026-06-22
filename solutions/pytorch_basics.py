b07314f2457274663e05a02d7e5cc23874682a6f
Tobias Nobile <tobias.nobile@hotmail.com>
2026-06-22 14:32:39 +0200

    Add bias with broadcasting

--- solutions/pytorch_basics.py
+++ solutions/pytorch_basics.py
@@ -17,3 +17,13 @@ def transpose_last_two(x: torch.Tensor) -> torch.Tensor:
     Swap the last two dimensions of x
     """
     return torch.transpose(x, -1, -2)
+
+def add_bias(x: torch.Tensor, b: torch.Tensor) -> torch.Tensor:
+    """
+    Add b to every row of x using broadcasting
+
+    How to check if dimensions are compatible for broadcasting ?
+    1. Aligner la dernière dimension de chaque tenseur sur la droite
+    2. Si elles sont égales ou si l'une d'elle vaut 1 ou est vide -> compatible
+    """
+    return x + b
