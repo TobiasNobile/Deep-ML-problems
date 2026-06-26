23dc15111b98ed2c54febc5f97e7b063ff36062d
Tobias Nobile <tobias.nobile@hotmail.com>
2026-06-26 18:43:52 +0200

    apply_adam_step_to_all_parameters

--- projects/Attention is All You Need - Transformer from Scratch/adam_optimizer_from_scratch.py
+++ projects/Attention is All You Need - Transformer from Scratch/adam_optimizer_from_scratch.py
@@ -23,3 +23,22 @@ def update_adam_second_moment(v_prev, grad, beta2):
 def apply_adam_bias_correction(m_t, v_t, beta1, beta2, step):
     """Return bias-corrected (m_hat, v_hat) for Adam at the given step."""
     return m_t / (1 - beta1**step), v_t / (1 - beta2**step)
+
+def apply_adam_step_to_all_parameters(parameter_list, optimizer_state, learning_rate, beta1=0.9, beta2=0.98, epsilon=1e-9):
+    """
+    Increment t, then for each param with a grad update m, v, bias-correct, and subtract delta in place.
+    """
+    optimizer_state["t"] += 1
+    for i in range(len(parameter_list)):
+        if parameter_list[i].grad is None:
+            continue
+        g = parameter_list[i].grad 
+        optimizer_state["m"][i] = beta1*optimizer_state["m"][i] + (1 - beta1)*g
+        optimizer_state["v"][i] = beta2*optimizer_state["v"][i] + (1 - beta2)*g**2
+
+        m_hat = optimizer_state["m"][i]/(1 - beta1**optimizer_state["t"])
+        v_hat = optimizer_state["v"][i]/(1 - beta2**optimizer_state["t"])
+
+        parameter_list[i].data = parameter_list[i].data - learning_rate * m_hat/(torch.sqrt(v_hat) + epsilon)
+
+    return optimizer_state 
