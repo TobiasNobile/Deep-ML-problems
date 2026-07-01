84a6f5773f783379438fffc6202d83eca6d80e6a
Tobias Nobile <tobias.nobile@hotmail.com>
2026-07-01 11:23:07 +0200

    Birthday Problem Probability

---
+++ solutions/246_birthday_problem_probability/solution.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def birthday_problem(n: int, days: int = 365) -> float:
+    """
+    Calculate the probability that at least two people share the same birthday.
+    
+    Args:
+        n: Number of people in the group
+        days: Number of days in a year (default 365)
+    
+    Returns:
+        float: Probability of at least one shared birthday, rounded to 4 decimal places
+    """
+    if n <= 1:
+        return 0
+    elif n > days:
+        return 1
+    P_no_matches = np.prod([(days - i)/days for i in range(n)])
+    return 1 - P_no_matches
